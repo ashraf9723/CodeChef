6d19e341c57fe37be8e5c946a26abc7132a738ea
MD ASHRAF <mdashraf9723@gmail.com>
2023-08-24 00:33:14 +0530

    Add files via upload

---
+++ PUZHUNT.py
@@ -0,0 +1,9 @@
+# cook your dish here
+# Read the number of people in Chef's team
+N = int(input())
+
+# Check if the team size is within the eligible range
+if 6 <= N <= 8:
+    print("Yes")
+else:
+    print("No")
